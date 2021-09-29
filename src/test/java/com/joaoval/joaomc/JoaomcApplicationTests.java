c05337fd56bda187372f7834ce66f892d240118c
Joao Vitor Alves Lima <joao-mex@hotmail.com>
2021-09-29 17:01:58 -0300

    Project created

---
+++ src/test/java/com/joaoval/joaomc/JoaomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaoval.joaomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoaomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
