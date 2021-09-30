c237ad20cc2c419e64baf1eb113f0520c563e1a7
Joao Vitor Alves Lima <joao-mex@hotmail.com>
2021-09-30 01:18:51 -0300

    Product and association ManytoMany

--- src/main/java/com/joaoval/joaomc/JoaomcApplication.java
+++ src/main/java/com/joaoval/joaomc/JoaomcApplication.java
@@ -1,7 +1,9 @@
 package com.joaoval.joaomc;
 
 import com.joaoval.joaomc.domain.Category;
+import com.joaoval.joaomc.domain.Product;
 import com.joaoval.joaomc.repositories.CategoryRepository;
+import com.joaoval.joaomc.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -15,6 +17,9 @@ public class JoaomcApplication implements CommandLineRunner {
 	@Autowired
 	private CategoryRepository categoryRepository;
 
+	@Autowired
+	private ProductRepository productRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(JoaomcApplication.class, args);
 	}
@@ -25,6 +30,18 @@ public class JoaomcApplication implements CommandLineRunner {
 		Category cat1 = new Category(null, "Informatics");
 		Category cat2 = new Category(null, "Office");
 
+		Product p1 = new Product(null, "Computer", 2000.00);
+		Product p2 = new Product(null, "Printer", 800.00);
+		Product p3 = new Product(null, "Mouse", 80.00);
+
+		cat1.getProducts().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProducts().addAll(Arrays.asList(p2));
+
+		p1.getCategories().addAll(Arrays.asList(cat1));
+		p2.getCategories().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategories().addAll(Arrays.asList(cat1));
+
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 }
--- src/main/java/com/joaoval/joaomc/domain/Category.java
+++ src/main/java/com/joaoval/joaomc/domain/Category.java
@@ -1,10 +1,9 @@
 package com.joaoval.joaomc.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -15,6 +14,9 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
+
+    @ManyToMany(mappedBy = "categories")
+    private List<Product> products = new ArrayList<>();
 
     public Category() {
     }
@@ -40,6 +42,13 @@ public class Category implements Serializable {
         this.name = name;
     }
 
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
