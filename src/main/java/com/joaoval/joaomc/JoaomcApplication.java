2dd2a74c7b4cf76c4b116ff99b277d7cfb869d7e
Joao Vitor Alves Lima <joao-mex@hotmail.com>
2021-09-30 11:50:48 -0300

    State and city

--- src/main/java/com/joaoval/joaomc/JoaomcApplication.java
+++ src/main/java/com/joaoval/joaomc/JoaomcApplication.java
@@ -1,9 +1,13 @@
 package com.joaoval.joaomc;
 
 import com.joaoval.joaomc.domain.Category;
+import com.joaoval.joaomc.domain.City;
 import com.joaoval.joaomc.domain.Product;
+import com.joaoval.joaomc.domain.State;
 import com.joaoval.joaomc.repositories.CategoryRepository;
+import com.joaoval.joaomc.repositories.CityRepository;
 import com.joaoval.joaomc.repositories.ProductRepository;
+import com.joaoval.joaomc.repositories.StateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -19,6 +23,12 @@ public class JoaomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private ProductRepository productRepository;
+
+	@Autowired
+	private StateRepository stateRepository;
+
+	@Autowired
+	private CityRepository cityRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(JoaomcApplication.class, args);
@@ -43,5 +53,18 @@ public class JoaomcApplication implements CommandLineRunner {
 
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+		State sta1 = new State(null, "Minas Gerais");
+		State sta2 = new State(null, "São Paulo");
+
+		City c1 = new City(null, "Uberlândia", sta1);
+		City c2 = new City(null, "São Paulo", sta2);
+		City c3 = new City(null, "Campinas", sta2);
+
+		sta1.getCities().addAll(Arrays.asList(c1));
+		sta2.getCities().addAll(Arrays.asList(c2, c3));
+
+		stateRepository.saveAll(Arrays.asList(sta1, sta2));
+		cityRepository.saveAll(Arrays.asList(c1, c2, c3));
 	}
 }
